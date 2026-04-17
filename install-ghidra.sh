798e5069b084bf772f4b4ce3ef82f54be9d71bbd
dan-acsc <130635354+dan-acsc@users.noreply.github.com>
2026-04-17 13:46:55 +1000

    feat: upgrade ghidra (#13)
    feat: upgrade ghidra
    
    
    [AB#4922](https://dev.azure.com/MSFT-ACSC/d647a5d6-6131-437d-81dc-fb931aec36b8/_workitems/edit/4922)
    
    ---------
    
    Co-authored-by: dan-acsc <dan-acsc@users.noreply.github.com>

--- install-ghidra.sh
+++ install-ghidra.sh
@@ -1,15 +1,19 @@
 # This scripts installs ghidra into the users /home/azul/.local/bin
 # This makes it available to run and avoids some of the complexity with finding the binary at runtime.
 
+# NOTE - the wget link is easier to update manually due to the date at the end.
+GHIDRA_VERSION=12.0.4
+
 set -e
 # Remove previous install
-rm -f ghidra_12.0_PUBLIC_*.zip
+rm -f ghidra_${GHIDRA_VERSION}_PUBLIC_*.zip
 rm -rf /usr/bin/ghidra
 
 # Download latest version of Ghidra (last updated October 2025)
-wget https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_12.0_build/ghidra_12.0_PUBLIC_20251205.zip
+# refer to https://github.com/NationalSecurityAgency/ghidra/releases - for latest
+wget https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_12.0.4_build/ghidra_12.0.4_PUBLIC_20260303.zip
 # Extract zip files and move it into /home/azul/.local/bin
-unzip -o ghidra_12.0_PUBLIC_*.zip
-rm -f ghidra_12.0_PUBLIC_*.zip
-mv ghidra_12.0_PUBLIC /usr/bin/ghidra
+unzip -o ghidra_${GHIDRA_VERSION}_PUBLIC_*.zip
+rm -f ghidra_${GHIDRA_VERSION}_PUBLIC_*.zip
+mv ghidra_${GHIDRA_VERSION}_PUBLIC /usr/bin/ghidra
 echo "ghidra installed"
